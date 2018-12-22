b5219cf636912f8ed90b7819085b33fb688643fe
Aaditya <addyrockx96@gmail.com>
2018-12-22 16:36:15 +0530

    1st deployment

--- getpostdb.php
+++ getpostdb.php
@@ -1,5 +1,4 @@
 <?php
-
-$conn = mysqli_connect('localhost','root','aaditya','techx')
+$conn = mysqli_connect('den1.mysql3.gear.host','techx','techx@456','techx')
  or die('Error connecting to MySQL server.');
- ?>
+?>
